1c01ae03228d2035a37940abce9e80ba6957190b
Alejandro Baquero <diegobaquero991@gmail.com>
2022-10-18 16:39:22 -0500

    13 de Octubre, Instalacion react

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Tienda te tecnologia BaqueroSystem</h1>
+        <img src="./images/BaqueroSystem.png" alt="Logo"></img>
+    </div>
+  );
+}
+
+export default App;
