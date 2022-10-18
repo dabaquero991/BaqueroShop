6fc6450cd457c8d48f9dd8bfd9c35b0928f55e33
Alejandro Baquero <diegobaquero991@gmail.com>
2022-10-17 20:21:16 -0500

    Clase 12 de octubre, controladores para productos y fetch

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,10 +1,69 @@
+//const productos = require("../models/productos");
 const producto=require("../models/productos")
+// usurpación del require
+const fetch =(url)=>import('node-fetch').then(({default:fetch})=>fetch(url));
 
 //Ver listas de productos
-exports.getProducts=(req,res,next) =>{
+exports.getProducts=async (req,res,next) =>{
+    const productos= await producto.find();
     res.status(200).json({
         success:true,
-        message: "En esta ruta ud va a poder ver todos los productos"
+        count: productos.length,
+        productos
+    })
+}
+
+// Ver producto por ID
+exports.getProductById= async(req, res, next)=>{
+    const product= await producto.findById(req.params.id)
+    if (!product){
+        return res.status(404).json({
+            success: false,
+            message: "Producto no encontrado"
+        })
+    }
+    res.status(200).json({
+        success:true,
+        message: "A continuación encontrará la información del producto: ",
+        product
+    })
+}
+
+// Update un producto
+exports.updateProduct= async (req, res, next)=>{
+    let product= await producto.findById(req.params.id) //variable de tipo modificable
+    if (!product){ // Verificó que el objeto no existe, para finalizar el proceso
+        return res.status(404).json({
+            success: false,
+            message: "Producto no encontrado"
+        })
+    }
+    // Si el objeto si existe, se ejecuta la actualización
+    product=await producto.findByIdAndUpdate(req.params.id, req.body, {
+        new:true, // Valida solo los atributos nuevos o actualizados
+        runValidators:true
+    });
+    // Respondo Ok si el producto si se actualizó
+    res.status(200).json({
+        success:true,
+        message: "Producto actualizado correctamente: ",
+        product
+    })
+}
+
+// Delete producto
+exports.deleteProduct= async (req, res, next)=>{
+    const product= await producto.findById(req.params.id) //variable de tipo modificable
+    if (!product){ // Verificó que el objeto no existe, para finalizar el proceso
+        return res.status(404).json({
+            success: false,
+            message: "Producto no encontrado"
+        })
+    }
+    await product.remove();
+    res.status(200).json({
+        success:true,
+        message: "Producto eliminado correctamente: "
     })
 }
 
@@ -16,3 +75,27 @@ exports.newProduct=async(req,res,next)=>{
         product
     })
 }
+
+
+//HABLEMOS DE FETCH
+//* Ver todos los productos
+function verProductos(){
+    fetch('http://localhost:4000/api/productos')
+    .then(res=>res.json())
+    .then(res=>console.log(res))
+    .catch(err=>console.error(err))
+}
+
+//verProductos(); //Llamamos al metodo creado para probar la consulta
+
+//* ver por Id
+function verProductoPorId(id){
+    fetch('http://localhost:4000/api/producto/'+id)
+    .then(res=>res.json())
+    .then(res=>console.log(res))
+    .catch(err=>console.error(err))
+}
+
+//verProductoPorId('634de32a7c22b3852ae39665'); // probamos metodo con un id
+
+
--- back/routes/products.js
+++ back/routes/products.js
@@ -2,9 +2,15 @@ const express=require("express")
 const router=express.Router();
 
 //Trae la respuesta json desde el controlador
-const {getProducts, newProduct} = require("../controllers/productsController") 
+const {getProducts, newProduct, getProductById, updateProduct, deleteProduct} = require("../controllers/productsController") 
 
 //Establece que ruta ver el getProducts
 router.route('/productos').get(getProducts) 
-router.route('/producto/nuevo').post(newProduct);
+router.route('/producto/nuevo').post(newProduct); //Ruta nuevo producto
+router.route('/producto/:id').get(getProductById); //Para consultar por id
+router.route('/producto/:id').put(updateProduct); //Creación de la ruta de actualización 
+router.route('/producto/:id').delete(deleteProduct); // Creación de la ruta de eliminacion por Id
+
+//fetch
+
 module.exports=router;
