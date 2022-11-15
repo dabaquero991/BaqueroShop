48c360862ff3ba98d192cafd2bbcd3549291cf51
Alejandro Baquero <diegobaquero991@gmail.com>
2022-11-14 19:40:40 -0500

    Clase 25/10/22 Detalles Product

--- front/src/actions/productActions.js
+++ front/src/actions/productActions.js
@@ -29,11 +29,11 @@ export const getProducts = () => async(dispatch)=>{
 }
 
 //Acciones para ver detalles del producto
-export const getProductsDetalis = (id) => async(dispatch)=>{
+export const getProductDetails = (id) => async(dispatch)=>{
     try {
         dispatch({type: PRODUCT_DETAILS_REQUEST})
 
-        const {data} = await axios.get(`api/productos/:${id}`)
+        const {data} = await axios.get(`/api/producto/${id}`)
 
         dispatch({
             type:PRODUCT_DETAILS_SUCCESS,
