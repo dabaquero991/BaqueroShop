f931699366d06dfa94c7734100a94cda813f4766
Alejandro Baquero <diegobaquero991@gmail.com>
2022-11-17 10:42:16 -0500

    Volver a usuarios

--- back/app.js
+++ back/app.js
@@ -1,6 +1,5 @@
 const express=require("express");
 const app = express();
-const errorMiddleware= require("./middleware/errors")
 
 app.use(express.json());
 
@@ -9,8 +8,5 @@ const productos=require("./routes/products")
 
 //Ruta del navegador (esta cambia)
 app.use('/api',productos)
-
-//MiddleWares para manejar errores
-app.use(errorMiddleware)
 
 module.exports=app
