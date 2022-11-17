5fc7f3237d971534365f35c7677d815be8970b22
Alejandro Baquero <diegobaquero991@gmail.com>
2022-11-17 10:43:13 -0500

    Volver a usuarios

--- front/src/actions/productActions.js
+++ front/src/actions/productActions.js
@@ -25,6 +25,28 @@ export const getProducts = () => async(dispatch)=>{
     }
 }
 
+<<<<<<< HEAD
+=======
+//Acciones para ver detalles del producto
+export const getProductsDetalis = (id) => async(dispatch)=>{
+    try {
+        dispatch({type: PRODUCT_DETAILS_REQUEST})
+
+        const {data} = await axios.get(`api/productos/:${id}`)
+
+        dispatch({
+            type:PRODUCT_DETAILS_SUCCESS,
+            payload: data.product
+        })
+    }catch (error){
+        dispatch({
+            type:PRODUCT_DETAILS_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+>>>>>>> parent of 48c3608 (Clase 25/10/22 Detalles Product)
 //clear error
 export const clearErrors = () => async(dispatch)=>{
     dispatch({
--- front/src/reducer/productReducer.js
+++ front/src/reducer/productReducer.js
@@ -1,5 +1,12 @@
 import { ALL_PRODUCTS_REQUEST, 
+<<<<<<< HEAD
     ALL_PRODUCTS_SUCCESS, 
+=======
+    ALL_PRODUCTS_SUCCESS,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL,
+>>>>>>> parent of 48c3608 (Clase 25/10/22 Detalles Product)
     ALL_PRODUCTS_FAIL,CLEAR_ERRORS } from "../constants/productConstants";
 
     export const productsReducer = (state = {products: []}, action)=>{
@@ -33,4 +40,40 @@ import { ALL_PRODUCTS_REQUEST,
                 
                 
         }
+<<<<<<< HEAD
+=======
+    }
+
+    // REDUCERS PARA DETALLES
+    export const productDetailsReducer = (state = {product: {}}, action)=>{
+        switch(action.type){
+            case PRODUCT_DETAILS_REQUEST:
+                    return{
+                        ...state,
+                        loading:true
+                    }
+
+                case PRODUCT_DETAILS_SUCCESS:
+                    return{
+                        loading:false,
+                        product: action.payload.product,
+                    }
+                case PRODUCT_DETAILS_FAIL:
+                    return{
+                        ...state,
+                        error: action.payload
+                    }
+                case CLEAR_ERRORS:{
+                    return{
+                        ...state,
+                        error:null
+                    }
+                }
+            
+            default:
+                return state;
+                
+                
+        }
+>>>>>>> parent of 48c3608 (Clase 25/10/22 Detalles Product)
     }
